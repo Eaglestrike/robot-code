ffd4e4d733412c9af88fb7d7669e5b06b87fd3ae
Josh Hejna <josh.hejna@gmail.com>
2019-01-29 19:01:39 -0800

    Add miminal main() code for Drive

--- c2018/src/main.rs
+++ c2018/src/main.rs
@@ -1,13 +1,19 @@
 mod config;
 mod subsystems;
 
-use crate::subsystems::drive::Drive;
 use bus::Bus;
 use crossbeam_channel::unbounded;
+use std::thread;
+use subsystems::drive::*;
+use subsystems::Subsystem;
 
 fn main() {
     let bus = Bus::new(0);
-    let (_, recv) = unbounded();
-    let _drive = Drive::new(bus, recv);
+    let (drive_send, recv) = unbounded();
+    thread::spawn(move || {
+        let drive = Drive::new(bus, recv);
+        drive.run();
+    });
+    drive_send.send(Instruction::GearShift(Gear::High)).unwrap();
     println!("Hello, world!");
 }
