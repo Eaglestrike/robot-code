ac598e1c8809fa55e938df817442f96029dce1ee
Aris (Karim) Merchant <wizkarim@gmail.com>
2019-02-04 15:33:23 -0800

    Add control code for drive subsystem

--- c2018/src/subsystems/controller/mod.rs
+++ c2018/src/subsystems/controller/mod.rs
@@ -2,38 +2,62 @@
 pub mod edge_detect;
 pub mod xbox;
 
-use crossbeam_channel::Sender;
-use super::{drive, Subsystem};
+use super::{
+    drive::{self, Gear},
+    Subsystem,
+};
 use crate::cheesy_drive::CheesyDrive;
+use crossbeam_channel::Sender;
 
 type Drive = Sender<drive::Instruction>;
 
 #[allow(dead_code)]
 struct Controller<T: Controls> {
     controls: T,
-    cheesy_drive: CheesyDrive,
-    drive: Drive
+    cheesy: CheesyDrive,
+    drive: Drive,
 }
 
 impl<T: Controls> Controller<T> {
     fn new(controls: T, drive: Drive) -> Self {
         Self {
             controls,
-            cheesy_drive: CheesyDrive::new(),
-            drive
+            cheesy: CheesyDrive::new(),
+            drive,
         }
     }
 }
 
 impl<T: Controls> Subsystem for Controller<T> {
-    fn run(self) {
+    fn run(mut self) {
+        let controls = self.controls;
 
+        // DRIVE
+        let wheel = controls.wheel();
+        let throttle = controls.throttle();
+        let quick_turn = controls.quick_turn();
+        let high_gear = !controls.low_gear();
+        if high_gear {
+            self.drive.send(drive::Instruction::GearShift(Gear::High))
+        } else {
+            self.drive.send(drive::Instruction::GearShift(Gear::Low))
+        }
+        .expect("Channel disconnected: ");
+
+        let signal = self
+            .cheesy
+            .cheesy_drive(wheel, throttle, quick_turn, high_gear);
+        self.drive
+            .send(drive::Instruction::Percentage(signal.l, signal.r))
+            .expect("Channel disconnected: ");
     }
 }
 
 pub trait Controls {
     fn throttle(&self) -> f64;
     fn wheel(&self) -> f64;
+    fn low_gear(&self) -> bool;
+    fn quick_turn(&self) -> bool;
     fn begin_ball_intake(&self) -> bool;
     fn abort_ball_intake(&self) -> bool;
     fn outtake_ball(&self) -> bool;
