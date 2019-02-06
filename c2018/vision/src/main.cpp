f9abace432945f31f8c722b0c3fef4e0d3fff3e0
Josh Hejna <josh.hejna@gmail.com>
2019-02-06 10:29:01 -0800

    Add box recognition

--- c2018/vision/src/main.cpp
+++ c2018/vision/src/main.cpp
@@ -25,6 +25,8 @@ int main(int argc, char **argv)
     ALGORITHM_PARAM(shigh, 255, 255);
     ALGORITHM_PARAM(vlow, 0, 255);
     ALGORITHM_PARAM(vhigh, 89, 255);
+    ALGORITHM_PARAM(minTargetRectArea, 100, 500);
+    ALGORITHM_PARAM(minTargetFullness, 500, 1000);
 
 #ifdef USE_CAMERA
     VideoCapture cam(0);
@@ -45,7 +47,10 @@ int main(int argc, char **argv)
     Mat blurred;
     Mat hsv;
     Mat mask;
-    // auto contours;
+    vector<vector<Point>> contours;
+    vector<Point> convex_cnt;
+    vector<Point> poly;
+    vector<RotatedRect> targets;
 
     for (;;)
     {
@@ -71,6 +76,57 @@ int main(int argc, char **argv)
 
         inRange(hsv, Scalar(hlow, slow, vlow), Scalar(hhigh, shigh, vhigh), mask);
         SHOW("mask", mask);
+
+        cout << "test" << endl;
+        contours.clear();
+        findContours(mask, contours, RETR_EXTERNAL, CHAIN_APPROX_TC89_KCOS);
+
+        drawContours(resized, contours, -1, Scalar(255, 255, 0));
+        SHOW("contours", resized);
+
+        targets.clear();
+        for (auto &cnt : contours)
+        {
+            // determine if contour is a valid target
+            convex_cnt.clear();
+            convexHull(cnt, convex_cnt);
+            RotatedRect rect = minAreaRect(convex_cnt);
+
+            if (rect.size.area() < minTargetRectArea)
+            {
+                cout << "area too small" << endl;
+                continue;
+            }
+            float area = static_cast<float>(contourArea(convex_cnt));
+            if (area / rect.size.area() < static_cast<float>(minTargetFullness) / 1000)
+            {
+                cout << "fullness too low" << endl;
+                continue;
+            }
+
+            targets.push_back(rect);
+            // store target info
+        }
+
+        for (RotatedRect &rect : targets)
+        {
+
+            // perform target matching
+            // output found vertical angle and horizontal angle
+            cv::Mat pts;
+            // vector<vector<Point>> vec2;
+            // vec2.push_back(v);
+            boxPoints(rect, pts);
+            // polylines(resized, pts, true, Scalar(0, 0, 255));
+
+            Point2f rect_points[4];
+            rect.points(rect_points);
+            for (int j = 0; j < 4; j++)
+                line(resized, rect_points[j], rect_points[(j + 1) % 4], Scalar(0, 0, 255), 1, 8);
+
+            cout << "test" << endl;
+        }
+        SHOW("boxes", resized);
 
         switch (waitKey(WAITKEY_DELAY))
         {
