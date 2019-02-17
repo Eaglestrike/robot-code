0220c04470c983c30f74d2b2aea1057808e09d57
Josh Hejna <josh.hejna@gmail.com>
2019-02-16 17:10:15 -0800

    Rework EdgeDetector
    * Add new methods that allow us to do work on rise and fall
    * Make the get( ) method take a boolean to make it more conducive to
    borrowing rules and wrapping the Controls Trait

--- c2018/src/subsystems/controller/edge_detect.rs
+++ c2018/src/subsystems/controller/edge_detect.rs
@@ -1,4 +1,4 @@
-#[derive(Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Edge {
     Falling,
     Rising,
@@ -15,45 +15,58 @@ impl Edge {
     pub fn flat(self) -> bool {
         self == Edge::Flat
     }
+
+    pub fn sig_send(self, mut f: impl FnMut(bool) -> ()) {
+        match self {
+            Edge::Rising => f(true),
+            Edge::Falling => f(false),
+            Edge::Flat => (),
+        };
+    }
+
+    pub fn sig_send_val<T>(self, rise: T, fall: T, mut f: impl FnMut(T) -> ()) {
+        match self {
+            Edge::Rising => f(rise),
+            Edge::Falling => f(fall),
+            Edge::Flat => (),
+        };
+    }
 }
 
-#[derive(Clone)]
-pub struct EdgeDetector<T: FnMut() -> bool> {
-    closure: T,
+#[derive(Debug, Clone)]
+pub struct EdgeDetector {
     last_value: bool,
 }
 
-impl<T: FnMut() -> bool> EdgeDetector<T> {
-    pub fn new(closure: T) -> Self {
+impl EdgeDetector {
+    pub fn new() -> Self {
         Self {
-            closure,
             last_value: false,
         }
     }
 
-    pub fn get(&mut self) -> Edge {
-        let new_value = (self.closure)();
-        let last_value = self.last_value;
-        self.last_value = new_value;
-
-        if last_value && !new_value {
+    pub fn get(&mut self, new_value: bool) -> Edge {
+        let edge =
+        if self.last_value && !new_value {
             Edge::Falling
-        } else if !last_value && new_value {
+        } else if !self.last_value && new_value {
             Edge::Rising
         } else {
             Edge::Flat
-        }
+        };
+        self.last_value = new_value;
+        edge
     }
 
-    pub fn falling(&mut self) -> bool {
-        self.get() == Edge::Falling
+    pub fn falling(&mut self, new_value: bool) -> bool {
+        self.get(new_value) == Edge::Falling
     }
 
-    pub fn rising(&mut self) -> bool {
-        self.get() == Edge::Rising
+    pub fn rising(&mut self, new_value: bool) -> bool {
+        self.get(new_value) == Edge::Rising
     }
 
-    pub fn flat(&mut self) -> bool {
-        self.get() == Edge::Flat
+    pub fn flat(&mut self, new_value: bool) -> bool {
+        self.get(new_value) == Edge::Flat
     }
 }
