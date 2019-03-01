59f8c8cb00bd07a528f2cc52cd484d8bd710e94e
Josh Hejna <josh.hejna@gmail.com>
2019-03-01 04:43:23 -0800

    Use v4l/by-path for the same effect as udev rules

--- c2018/vision/src/del_mar_quick_cams.cpp
+++ c2018/vision/src/del_mar_quick_cams.cpp
@@ -17,31 +17,31 @@ int main(int argc, char **argv)
     for (const auto &addr : cs::GetNetworkInterfaces())
         wpi::outs() << "  " << addr << '\n';
 
-    char buf[512];
-    static_assert(sizeof(buf) == 512, "MISMATCHED SIZEOF");
-    memset(buf, 0, sizeof(buf));
-    int count = readlink(CAM_FORWARD_ID.c_str(), buf, sizeof(buf) - 1);
-    if (count <= 0) {
-        wpi::outs() << "Could not resolve link to " << CAM_FORWARD_ID << '\n';
-        return 1;
-    }
-    std::string camForwardResolved(buf);
+    // char buf[512];
+    // static_assert(sizeof(buf) == 512, "MISMATCHED SIZEOF");
+    // memset(buf, 0, sizeof(buf));
+    // int count = readlink(CAM_FORWARD_ID.c_str(), buf, sizeof(buf) - 1);
+    // if (count <= 0) {
+    //     wpi::outs() << "Could not resolve link to " << CAM_FORWARD_ID << '\n';
+    //     return 1;
+    // }
+    // std::string camForwardResolved(buf);
 
-    memset(buf, 0, sizeof(buf));
-    count = readlink(CAM_REVERSE_ID.c_str(), buf, sizeof(buf) - 1);
-    if (count <= 0) {
-        wpi::outs() << "Could not resolve link to " << CAM_REVERSE_ID << '\n';
-        return 1;
-    }
-    std::string camReverseResolved(buf);
+    // memset(buf, 0, sizeof(buf));
+    // count = readlink(CAM_REVERSE_ID.c_str(), buf, sizeof(buf) - 1);
+    // if (count <= 0) {
+    //     wpi::outs() << "Could not resolve link to " << CAM_REVERSE_ID << '\n';
+    //     return 1;
+    // }
+    // std::string camReverseResolved(buf);
 
-    UsbCamera fcam{"ForwardCamera", camForwardResolved};
+    UsbCamera fcam{"ForwardCamera", CAM_FORWARD_ID};
     fcam.SetVideoMode(cs::VideoMode::kMJPEG, MJPEG_WIDTH, MJPEG_HEIGHT, MJPEG_FPS);
     cs::MjpegServer fMjpegServer{"ForwardHTTPMjpeg", MJPEG_FORWARD_PORT};
     fMjpegServer.SetSource(fcam);
     fMjpegServer.SetFPS(MJPEG_FPS);
 
-    UsbCamera rcam{"ReverseCamera", camReverseResolved};
+    UsbCamera rcam{"ReverseCamera", CAM_REVERSE_ID};
     fcam.SetVideoMode(cs::VideoMode::kMJPEG, MJPEG_WIDTH, MJPEG_HEIGHT, MJPEG_FPS);
     cs::MjpegServer rMjpegServer{"ReverseHTTPMjpeg", MJPEG_REVERSE_PORT};
     rMjpegServer.SetSource(rcam);
