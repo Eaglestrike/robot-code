356958bad6814842297ba02274d1bf2a6c55a294
Josh Hejna <josh.hejna@gmail.com>
2019-03-01 16:24:19 -0800

    Update ids for use with USB hub

--- c2018/vision/src/config.hpp
+++ c2018/vision/src/config.hpp
@@ -12,8 +12,8 @@ namespace c2019
 namespace vision
 {
 
-const std::string CAM_FORWARD_ID("/dev/v4l/by-path/platform-70090000.xusb-usb-0:3:1.0-video-index0");
-const std::string CAM_REVERSE_ID("/dev/v4l/by-path/platform-70090000.xusb-usb-0:4:1.0-video-index0");
+const std::string CAM_FORWARD_ID("/dev/v4l/by-path/platform-70090000.xusb-usb-0:4:1.0-video-index0");
+const std::string CAM_REVERSE_ID("/dev/v4l/by-path/platform-70090000.xusb-usb-0:3.3:1.0-video-index0");
 
 const int MJPEG_FORWARD_PORT = 5808;
 const int MJPEG_REVERSE_PORT = 5809;
