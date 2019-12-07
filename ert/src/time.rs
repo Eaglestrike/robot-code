45e72ac91bce8cfc7d4284d0eecdad699305db91
Josh Hejna <josh.hejna@gmail.com>
2019-12-07 01:30:20 -0800

    Test Duration <=> timespec conversion

--- ert/src/time.rs
+++ ert/src/time.rs
@@ -2,8 +2,36 @@ use libc::{clock_gettime, timespec, CLOCK_MONOTONIC};
 pub use std::time::{Duration, Instant};
 
 #[inline]
-fn timespec_to_duration(t: timespec) -> Duration {
+pub(crate) fn timespec_to_duration(t: timespec) -> Duration {
     Duration::from_secs(t.tv_sec as _) + Duration::from_nanos(t.tv_nsec as _)
+}
+
+#[inline]
+pub(crate) fn duration_to_timespec(t: Duration) -> timespec {
+    timespec {
+        tv_sec: t.as_secs() as _,
+        tv_nsec: t.subsec_nanos().into(),
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn duration_conversions() {
+    fn test(t: Duration) {
+        let c = timespec_to_duration(duration_to_timespec(t));
+        assert_eq!(t, c);
+        let ts1 = duration_to_timespec(t);
+        let ts2 = duration_to_timespec(timespec_to_duration(ts1));
+        assert_eq!(ts1, ts2);
+    }
+    [
+        Duration::from_millis(1043120),
+        Duration::from_micros(10121),
+        Duration::from_nanos(1123),
+        Duration::from_secs(121),
+    ]
+    .into_iter()
+    .for_each(|d| test(*d));
 }
 
 // TODO: benchmark overhead vs std::time::Instant (has more indirection, but inlining).
