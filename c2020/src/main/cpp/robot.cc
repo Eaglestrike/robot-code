f05e718645c8acfeac1433a69c8213cbda546567
Josh Hejna <josh.hejna@gmail.com>
2020-01-22 10:30:25 -0800

    Add more overrides to Robot

--- c2020/src/main/cpp/robot.cc
+++ c2020/src/main/cpp/robot.cc
@@ -5,9 +5,11 @@
 namespace team114 {
 namespace c2020 {
 
-Robot::Robot() : frc::TimedRobot{10_ms}, drive_{Drive::GetInstance()} {}
+Robot::Robot()
+    : frc::TimedRobot{Robot::kPeriod}, drive_{Drive::GetInstance()} {}
 
 void Robot::RobotInit() {}
+void Robot::RobotPeriodic() { drive_.Periodic(); }
 
 void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
@@ -17,6 +19,9 @@ void Robot::TeleopPeriodic() {}
 
 void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
 
 }  // namespace c2020
 }  // namespace team114
--- c2020/src/main/cpp/robot.h
+++ c2020/src/main/cpp/robot.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <frc/TimedRobot.h>
+#include <units/units.h>
 
 #include "subsystems/drive.h"
 #include "util/constructor_macros.h"
@@ -11,8 +12,10 @@ namespace c2020 {
 class Robot : public frc::TimedRobot {
     DISALLOW_COPY_ASSIGN(Robot)
    public:
+    inline static const auto kPeriod = 10_ms;
     Robot();
     void RobotInit() override;
+    void RobotPeriodic() override;
 
     void AutonomousInit() override;
     void AutonomousPeriodic() override;
@@ -22,6 +25,9 @@ class Robot : public frc::TimedRobot {
 
     void TestInit() override;
     void TestPeriodic() override;
+
+    void DisabledInit() override;
+    void DisabledPeriodic() override;
 
    private:
     Drive& drive_;
