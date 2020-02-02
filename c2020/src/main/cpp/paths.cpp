4a4064214e6ffe018192cf4450cea09309aba2f0
Josh Hejna <josh.hejna@gmail.com>
2020-02-02 12:55:28 -0800

    Cache trajectory calculations

--- c2020/src/main/cpp/paths.cpp
+++ c2020/src/main/cpp/paths.cpp
@@ -5,6 +5,13 @@
 
 #include "config.h"
 
+#define CACHING_PATH_BODY(body)                             \
+    static std::optional<frc::Trajectory> __cached;         \
+    if (!__cached.has_value()) {                            \
+        __cached.emplace(([]() -> frc::Trajectory body)()); \
+    }                                                       \
+    return __cached.value();
+
 namespace team114 {
 namespace c2020 {
 namespace paths {
@@ -26,15 +33,17 @@ frc::TrajectoryConfig MakeDefaultConfig() {
 }
 
 frc::Trajectory TestPath() {
-    auto cfg = MakeDefaultConfig();
-    return frc::TrajectoryGenerator::GenerateTrajectory(
-        Pose2d{0.0_m, 0.0_m, {0.0_rad}},
-        {
-            {0.5_m, 1.0_m},
-            {1.0_m, 1.0_m},
-            {1.5_m, 1.0_m},
-        },
-        Pose2d{2.0_m, 0.0_m, {0.0_rad}}, cfg);
+    CACHING_PATH_BODY({
+        auto cfg = MakeDefaultConfig();
+        return frc::TrajectoryGenerator::GenerateTrajectory(
+            Pose2d{0.0_m, 0.0_m, {0.0_rad}},
+            {
+                {0.5_m, 1.0_m},
+                {1.0_m, 1.0_m},
+                {1.5_m, 1.0_m},
+            },
+            Pose2d{2.0_m, 0.0_m, {0.0_rad}}, cfg);
+    })
 }
 
 }  // namespace paths
