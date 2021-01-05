059c3cc1f38719850fe990a09b0deed542b30722
SusanC3 <47490118+SusanC3@users.noreply.github.com>
2021-01-04 20:56:14 -0800

    Add documentation to 2nd half of config.h

--- c2020/src/main/cpp/config.h
+++ c2020/src/main/cpp/config.h
@@ -82,64 +82,64 @@ struct HoodConfig {
 };
 
 struct ShooterConifg {
-    int master_id;
-    int slave_id;
-    double shooter_current_limit;
-    double kF;
-    double kP;
-    double kI;
-    double kD;
-    VelocityMeasPeriod meas_period;
-    int meas_filter_width;
-    double shootable_err_pct;
-    int kicker_id;
-    double kicker_current_limit;
-    double kicker_cmd;
+    int master_id; /**< Each motor has an ID. There are 2 shooter motors performing the same action, and this is the master ID. */
+    int slave_id; /**< The respective slave ID. */
+    double shooter_current_limit; /**< The current limit (in amps) of the talons */
+    double kF; /**< F (feedforward) term in shooter PID */
+    double kP; /**< P term in shooter PID */
+    double kI; /**< I term in shooter PID */
+    double kD; /**< D term in shooter PID */
+    VelocityMeasPeriod meas_period; /**< Period of time over which velocidty is measured */
+    int meas_filter_width; /**< Not sure, is there some sort of filter on the shooter? */
+    double shootable_err_pct; /**< Perhaps ammount of error acceptable when trying to make a shot? */
+    int kicker_id; /**< Perhaps another talon motor that could be enabled to help the shooter wheel spin faster? */
+    double kicker_current_limit; /**< Current limit (amps) for kicker talon */
+    double kicker_cmd; /**< Percent output for kicker talon when engaged */
 };
 
 struct BallChannelConfig {
-    int serializer_id;
-    int channel_id;
-    double current_limit;
-    double serializer_cmd;
-    double channel_cmd;
-    int s0_port;
-    int s1_port;
-    int s2_port;
-    int s3_port;
+    int serializer_id; /**< Talon ID of serializing(?) talon motor **/
+    int channel_id; /**< Talon ID of another talon motor, perhaps to help channel the balls? **/
+    double current_limit; /**< The current limit (amps) of the talons **/
+    double serializer_cmd; /**< Percent output to serializer talon for it to serialize? **/
+    double channel_cmd; /**< Percent output to channel motor for it to channel? **/
+    int s0_port; /**< I'm guessing this would be one of 4 sensors in the serializer, perhaps to gain info on state of balls. **/
+    int s1_port; /**< I'm guessing this would be one of 4 sensors in the serializer, perhaps to gain info on state of balls. **/
+    int s2_port; /**< I'm guessing this would be one of 4 sensors in the serializer, perhaps to gain info on state of balls. **/
+    int s3_port; /**< I'm guessing this would be one of 4 sensors in the serializer, perhaps to gain info on state of balls. **/
 };
 
 struct ClimberConfig {
-    int master_id;
-    int slave_id;
-    double current_limit;
+    int master_id; /**< ID of master talon motor **/
+    int slave_id; /**< ID of slave talon motor **/
+    double current_limit; /**< The current limit (amps) of the talons **/
     // spool winding makes this weird
-    double avg_ticks_per_inch;
-    int release_solenoid_id;
-    int brake_solenoid_id;
-    int initial_step_ticks;
-    int forward_soft_limit_ticks;
-    double ascend_command;
-    double descend_command;
+    double avg_ticks_per_inch; /**< Probably number of ticks on motors that pass for climber to raise one inch. **/
+    int release_solenoid_id; /**< ID of solenoid that controls the release pneumatic (to raise/lower climber) **/
+    int brake_solenoid_id; /**< ID of brake solenoid, I think it's purpose is to hold the climber in position? **/
+    int initial_step_ticks; /**< Probably the amount of ticks the motors should turn initially to raise the climber. **/
+    int forward_soft_limit_ticks; /**< Probably a soft limit for how many ticks the motors should turn. **/
+    double ascend_command; /**< Percent output to talons to raise climber (positive value) **/
+    double descend_command; /**< Percent output to talons to lower climber (negative value) **/
 };
 
 struct LimelightConfig {
-    std::string name;
-    std::string table_name;
-    units::meter_t diff_height;
-    units::radian_t angle_above_horizontal;
+    std::string name; /**< Unused? Name of limelight**/
+    std::string table_name; /**< Name used for limelight NetworkTable**/
+    units::meter_t diff_height; /**< Not sure, unused **/
+    units::radian_t angle_above_horizontal; /**< Probaly offset, camera won't be exactly level **/
 };
 
 struct RobotConfig {
-    std::string mac_address;
-    DriveConfig drive;
-    ControlPanelConfig ctrl_panel;
-    IntakeConfig intake;
-    HoodConfig hood;
-    ShooterConifg shooter;
-    BallChannelConfig ball_channel;
-    ClimberConfig climber;
-    LimelightConfig limelight;
+    std::string mac_address; /**< For identifying roboRIO **/
+    DriveConfig drive; /**< For the drive base, go to DriveConfig for more info **/
+    ControlPanelConfig ctrl_panel; /**< For control panel, go to ControlPanelConfig for more info **/
+    IntakeConfig intake; /**< For the intake, go to IntakeConfig for more info **/
+    HoodConfig hood; /**< For the shooter hood, go to HoodConfig for more info **/
+    ShooterConifg shooter; /**< Perhaps scrapped, but for entire shooter. Go to ShooterConifg for more info **/
+    BallChannelConfig ball_channel; /**< For the ball channel, go to BallChannelConfig for more info **/
+    ClimberConfig climber; /**< For the climber, go to ClimberConfig for more info **/
+    LimelightConfig limelight; /**< For the limelight, go to LimelightConfig for more info **/
 };
 
 RobotConfig& GetConfig();
