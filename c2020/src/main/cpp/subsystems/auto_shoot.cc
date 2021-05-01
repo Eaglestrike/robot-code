2ff82c48cca31b17cadf7d23aa121a580bc461dd
SusanC3 <47490118+SusanC3@users.noreply.github.com>
2021-05-01 14:34:32 -0700

    Added test data, and dist to settings function

--- c2020/src/main/cpp/subsystems/auto_shoot.cc
+++ c2020/src/main/cpp/subsystems/auto_shoot.cc
@@ -3,11 +3,37 @@
 #define PI 3.14159265
 #define INF pow(10, 10); //why not?
 
-std::map<double, std::pair<double, double>> distance_to_settings; 
+//distance in meters
+std::map<double, std::pair<double, double>> distance_to_settings_map; 
+
+void auto_shoot_init() {
+	distance_to_settings_map.insert({ 2.6162 , {30.0, 30000.0} });
+	distance_to_settings_map.insert({ 3.1115 , {26.0, 34000.0} });
+	distance_to_settings_map.insert({ 3.8227 , {28.0, 30000.0} });
+	distance_to_settings_map.insert({ 4.0259 , {20.0, 35000.0} });
+	distance_to_settings_map.insert({ 4.5212 , {25.0, 40000.0} });	
+	distance_to_settings_map.insert({ 5.5626 , {20.0, 38000.0} });
+	distance_to_settings_map.insert({ 6.6929 , {20.0, 37700.0} });
+	distance_to_settings_map.insert({ 7.5946 , {20.0, 40000.0} });
+	distance_to_settings_map.insert({ 8.1534 , {20.0, 40000.0} });
+	distance_to_settings_map.insert({ 9.5123 , {20.0, 45000.0} });
+}
+
+std::pair<double, double> distance_to_settings(double dist) {
+	std::pair<double, double> best_combo;
+	double best_dist = 0;
+	for (auto const& x : distance_to_settings_map) {
+		if (x.first <= best_dist) {
+			best_combo = x.second;
+			best_dist = x.first;
+		}
+	}
+	return best_combo;
+}
 
 double distance(double y_off) {// x (horizontal) distance to goal
 
-	double shooter_height = 3.10; //310 cm tall
+	double shooter_height = 2.86; //310 cm tall
 	double cam_height = 0.55; //taken from config.cc
 //	double cam_angle = 8.0; //in degrees, taken from config.cc
 	double angle_to_top = y_off; //in degrees???
@@ -20,7 +46,7 @@ double distance(double y_off) {// x (horizontal) distance to goal
 //.first is hood angle, .second is flywheel speed
 std::pair<double, double> auto_shoot_calc(std::shared_ptr<nt::NetworkTable> network_table) {
     double y_off = network_table->GetNumber("ty", 0.0);
-	return distance_to_settings[distance(y_off)];
+	return distance_to_settings(distance(y_off));
 }
 
 //unfinished
@@ -47,7 +73,7 @@ std::pair<double, double> auto_shoot_calc(std::shared_ptr<nt::NetworkTable> netw
 /*POSSIBLE THINGS TO LOOK FOR IN DEBUG:
 	- something is in degrees/radians and I thought otherwise
 	- include issues (network table, limelight)
-	- hardware (limelight, motors, etc) being fucked in general
+	- the motors or the limelight is being stupid
 	- 
 	- 
 */
