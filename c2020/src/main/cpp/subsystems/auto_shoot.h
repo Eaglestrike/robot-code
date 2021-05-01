601941da38f11ccee7f0b9ad31f3041ce4227229
SusanC3 <47490118+SusanC3@users.noreply.github.com>
2021-05-01 14:35:43 -0700

    to go with auto_shoot.cc

--- c2020/src/main/cpp/subsystems/auto_shoot.h
+++ c2020/src/main/cpp/subsystems/auto_shoot.h
@@ -5,6 +5,8 @@
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
 
+void auto_shoot_init();
+std::pair<double, double> distance_to_settings(double dist);
 double distance(double y_off);
 std::pair<double, double> auto_shoot_calc(std::shared_ptr<nt::NetworkTable> network_table);
 void center(std::shared_ptr<nt::NetworkTable> network_table);
