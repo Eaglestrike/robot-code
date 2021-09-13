5bf76fbcb84d4ffaab813d75e931b4daf5916776
AndrewKim1S <54419795+AndrewKim1S@users.noreply.github.com>
2021-09-13 14:39:33 -0700

    Add files via upload

---
+++ c2021 v2/src/main/include/Robot.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/Joystick.h>
+#include <frc/XboxController.h>
+#include <frc/Timer.h>
+
+#include "ctre/Phoenix.h"
+#include "frc/WPILib.h"
+#include <iostream>
+
+#include <frc/SpeedControllerGroup.h>
+#include <frc/controller/PIDController.h>
+#include <frc/kinematics/DifferentialDriveKinematics.h>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
+#include <frc/drive/DifferentialDrive.h>
+
+#include "Drive.h"
+#include "Shoot.h"
+
+//Drive
+frc::Joystick l_joy{0};
+frc::Joystick r_joy{1};
+frc::XboxController xbox{2};
+
+class Robot : public frc::TimedRobot {
+  public:
+    void RobotInit() override;
+    void RobotPeriodic() override;
+    void AutonomousInit() override;
+    void AutonomousPeriodic() override;
+    void TeleopInit() override;
+    void TeleopPeriodic() override;
+    void DisabledInit() override;
+    void DisabledPeriodic() override;
+    void TestInit() override;
+    void TestPeriodic() override;
+
+  private:
+    frc::SendableChooser<std::string> m_chooser;
+    const std::string kAutoNameDefault = "Default";
+    const std::string kAutoNameCustom = "My Auto";
+    std::string m_autoSelected;
+
+  Drive Drivetrain;
+  Shoot Shooter;
+};
