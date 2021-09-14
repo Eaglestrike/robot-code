d99e855d582842c3508331b8eff679102300072c
Susan Clay <47490118+SusanC3@users.noreply.github.com>
2021-09-14 16:21:19 -0700

    Add limelight, shooter status, auto aim

---
+++ 2021_v2/src/main/include/Limelight.h
@@ -0,0 +1,16 @@
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+
+class Limelight{
+    public:
+        Limelight();
+        double getXOff();
+        double getYOff();
+        std::shared_ptr<nt::NetworkTable> GetNetworkTable();
+    private:
+        void ReadPeriodicIn();
+        std::shared_ptr<nt::NetworkTable> network_table;
+        std::string table_name = "limelight";
+        double height;
+        double angle_above_horizontal;
+};
