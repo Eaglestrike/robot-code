e5d9f66d5e44076d8bd8b13078349701ed93c05f
acheng-mvla <100023295@mvla.net>
2021-09-13 22:26:34 -0700

    operator file skeleton

--- c2021 v2/src/main/include/Operator.h
+++ c2021 v2/src/main/include/Operator.h
@@ -6,9 +6,9 @@ class Operator {
 	public:
 		Operator(Shooter s, Intake i, Climb c);
 		void periodic(XboxController xbox);
-		void shoot();
-		void intake();
-		void climb();
+		void shoot(double x);
+		void intake(bool active);
+		void climb(bool active);
 	private:
 		Shoot shoot_;
 		Intake intake_;
