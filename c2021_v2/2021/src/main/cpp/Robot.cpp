6ea12fba5f503dd0cb13d77f3886ffa2c45a488d
AndrewKim1S <54419795+AndrewKim1S@users.noreply.github.com>
2021-09-14 18:13:20 -0700

    Add files via upload

---
+++ c2021_v2/2021/src/main/cpp/Robot.cpp
@@ -0,0 +1,34 @@
+#include "Robot.h"
+
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+}
+
+void Robot::RobotPeriodic() {
+
+}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+void Robot::TeleopInit() {}
+
+void Robot::TeleopPeriodic() {
+  //Currently set to Manual Mode for testing
+  _control.Manual(xbox, l_joy, r_joy);
+  //uncomment the following for periodic mode
+  //_control.Periodic(xbox, l_joy, r_joy);
+}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ c2021_v2/2021/src/main/cpp/Shoot.cpp
@@ -0,0 +1,66 @@
+#include "Shoot.h"
+#include <string>
+
+Shoot::Shoot(){
+    // shoot_master->Set(TalonFXInvertType::Clockwise);
+    // shoot_slave->Set(TalonFXInvertType::Clockwise);
+    shoot_slave->Follow(*shoot_master);
+}
+
+//Limelight aiming and stuff
+//We can't even test due to no serializer so... idk have percent output on dashboard when we test
+void Shoot::Periodic(){
+    if (state == State::Idle) {
+        //reset? idk
+    } 
+    if (state == State::Aiming) {
+        Aim();
+    }
+    if (state == Shooting) {
+        //set hood
+        // set flywheel
+    }
+}
+
+void Shoot::setState(Shoot::State newState) {
+    state = newState;
+}
+
+void Shoot::Aim(){
+    double Kp = 0.017; 
+    double Ki = 0.015;
+	double x_off = limelight->getXOff();
+
+	double power = Kp*x_off + Ki;
+    if (power < -0.99) power = -0.99;
+    if (power > 0.99) power = 0.99;
+  //  std::cout <<"x offset: " << x_off << std::endl; 
+   // std::cout << "steering adjust: " << steering_adjust << std::endl; 
+    turret->Set(ControlMode::PercentOutput, power);
+}
+
+void Shoot::Auto(){}
+
+void Shoot::Manual(const frc::XboxController & xbox){
+    double shoot_value = xbox.GetY(frc::GenericHID::kLeftHand);
+    double turret_rot = xbox.GetY(frc::GenericHID::kRightHand);
+
+    double hood_value = xbox.GetX(frc::GenericHID::kRightHand);
+
+    //deadband
+    if(shoot_value <= 0.05){
+        shoot_value = 0;
+    }
+    if(abs(turret_rot) <= 0.05){
+        turret_rot = 0;
+    }
+    if(abs(hood_value) <= 0.05){
+        hood_value = 0;
+    }
+
+    shoot_master->Set(ControlMode::PercentOutput, shoot_value*2);
+    shoot_slave->Set(ControlMode::PercentOutput, -shoot_value*2);
+    turret->Set(ControlMode::PercentOutput, -turret_rot*0.15);
+
+
+};
