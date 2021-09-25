5cf4cc040b5ae50dcc5cc4bd96fe1b16c9a1c154
Susan Clay <47490118+SusanC3@users.noreply.github.com>
2021-09-25 16:20:54 -0700

    improve interpolation

--- 2021_v2/robot-code/2021/src/main/cpp/AutoShoot.cpp
+++ 2021_v2/robot-code/2021/src/main/cpp/AutoShoot.cpp
@@ -5,31 +5,41 @@
 #define INF pow(10, 10); //why not?
 
 double y_off = 0;
+double max_dist = 7.42; //last key in map
 
 
 //distance in meters
 std::map<double, AutoShoot::Settings> distance_to_settings_map; 
 
 AutoShoot::AutoShoot() {
-	distance_to_settings_map.insert({ 3.28 , {25.0, 35000.0, 0.7} });
-	distance_to_settings_map.insert({ 3.86 , {22.0, 35000.0, 0.7} });
-	distance_to_settings_map.insert({ 4.67 , {20.0, 35000.0, 0.7} });
-	distance_to_settings_map.insert({ 5.91 , {20.0, 40000.0, 0.7} });
-	distance_to_settings_map.insert({ 6.24 , {20.0, 37000.0, 0.75} });
-	distance_to_settings_map.insert({ 7.42 , {20.0, 40000.0, 0.8} });
+	distance_to_settings_map.insert({ 3.28 , (Settings){25.0, 35000.0, 0.7} });
+	distance_to_settings_map.insert({ 3.86 , (Settings){22.0, 35000.0, 0.7} });
+	distance_to_settings_map.insert({ 4.67 , (Settings){20.0, 35000.0, 0.7} });
+	distance_to_settings_map.insert({ 5.91 , (Settings){20.0, 40000.0, 0.7} });
+	distance_to_settings_map.insert({ 6.24 , (Settings){20.0, 37000.0, 0.75} });
+	distance_to_settings_map.insert({ 7.42 , (Settings){20.0, 40000.0, 0.8} });
 }
 
-//replace with interpolation code?
 AutoShoot::Settings distance_to_settings(double dist) {
 	AutoShoot::Settings best_combo;
-	double best_dist = 0;
-	for (auto x : distance_to_settings_map) {
-		if (abs(x.first-dist) <= abs(best_dist-dist)) {
-			best_dist = x.first;
-			best_combo = x.second;
-		}
+	std::map<double, AutoShoot::Settings>::iterator it;
+	it = distance_to_settings_map.begin();
+	for (it = distance_to_settings_map.begin(); it != distance_to_settings_map.end(); it++) //iterate through settings
+	{
+		double prevDist = it->first;
+		double nextDist = (std::next(it, 1))->first;
+		AutoShoot::Settings prevSetting = it->second;
+		AutoShoot::Settings nextSetting = (std::next(it, 1))->second;
+        if (dist < prevDist) return prevSetting; //min threshold
+		if (prevDist <= dist && dist <= nextDist) { //we want something between these settings
+			//it's the previous setting plus the difference to the next setting times the percent (0-1) to the next setting the input distance is
+			best_combo.flywheel_speed = prevSetting.flywheel_speed + (nextSetting.flywheel_speed - prevSetting.flywheel_speed)*((dist-prevDist)/(nextDist-prevDist));
+			best_combo.kicker_speed = prevSetting.kicker_speed + (nextSetting.kicker_speed - prevSetting.kicker_speed)*((dist-prevDist)/(nextDist-prevDist));
+			best_combo.hood_angle = prevSetting.hood_angle + (nextSetting.hood_angle - prevSetting.hood_angle)*((dist-prevDist)/(nextDist-prevDist));
+            return best_combo;
+        }
 	}
-	return best_combo;
+	return distance_to_settings_map[max_dist];
 }
 
 double distance(Limelight& limelight) {// x (horizontal) distance to goal
@@ -50,3 +60,4 @@ AutoShoot::Settings AutoShootCalc(std::shared_ptr<nt::NetworkTable> network_tabl
 	else std::cout << "doesn't see it" << std::endl;
 	return distance_to_settings(distance(limelight));
 }
+
