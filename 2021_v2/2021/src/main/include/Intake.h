b14a5933dd35f22b57952f54a0b8a9a4f74b691d
AndrewKim1S <54419795+AndrewKim1S@users.noreply.github.com>
2021-09-28 12:30:30 -0700

    Add files via upload

---
+++ 2021_v2/2021/src/main/include/Intake.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <ctre/Phoenix.h>
+#include <frc/WPILib.h>
+#include <frc/Solenoid.h>
+
+
+class Intake{
+    public:
+        void Deploy();
+        void Run();
+        void Retract();
+        void Unjam();
+        bool Deployed();
+        
+    private:
+
+        /*WPI_TalonFX * intake_motor = new WPI_TalonFX(9);
+        frc::Solenoid right_pneumatic{0};
+        frc::Solenoid left_pneumatic{1};
+        bool deployed = false;*/
+};
