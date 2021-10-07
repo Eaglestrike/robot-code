2bd9812d825c51c063f598f3959bba0f3bdb93bc
AndrewKim1S <54419795+AndrewKim1S@users.noreply.github.com>
2021-10-07 15:15:45 -0700

    Add files via upload

---
+++ 2021_v2/2021/src/main/cpp/Robot.cpp
@@ -0,0 +1,150 @@
+#include "Robot.h"
+
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+  //camera = frc::CameraServer::GetInstance()->StartAutomaticCapture();
+}
+
+
+void Robot::RobotPeriodic() {
+  compressor.Start();
+}
+
+
+void Robot::AutonomousInit() {
+  _shooter.Zero();
+  Auto_timer.Reset();
+  Auto_timer.Start();
+}
+
+
+void Robot::AutonomousPeriodic() {
+  if(Auto_timer.Get() < 2){
+    _drivetrain.Auto();
+  } 
+  else if(Auto_timer.Get() > 3 && Auto_timer.Get() < 5){
+    _shooter.Aim();
+    _shooter.setState(Shoot::State::Shooting);
+  }
+  else if(Auto_timer.Get() > 5 && Auto_timer.Get() < 7){
+    _channel.setState(Channel::State::Shooting);
+  }
+  else if(Auto_timer.Get() > 10){
+    _shooter.setState(Shoot::State::Idle);
+    _channel.setState(Channel::State::Idle);
+  }
+
+  _shooter.Periodic();
+  _channel.Periodic();
+}
+
+
+void Robot::TeleopInit() {
+  Auto_timer.Reset();
+  Auto_timer.Stop();
+}
+
+
+void Robot::TeleopPeriodic() {
+  //Drive & Manual turret movement
+  _shooter.Manual_Turret(xbox.GetX(frc::GenericHID::kRightHand));
+  _drivetrain.Periodic(l_joy.GetRawAxis(1), -1*r_joy.GetRawAxis(0));
+  
+  //Aim Button A
+  if(xbox.GetRawButton(1)){
+      _shooter.setState(Shoot::State::Aiming);
+      _channel.setState(Channel::State::Idle);
+      _intake.setState(Intake::State::Idle);
+  }
+
+  //Shoot Button B
+  else if(xbox.GetRawButton(2)) {
+    _shooter.setState(Shoot::State::Shooting);
+    _channel.setState(Channel::State::Idle);
+    Auto_timer.Start();
+      if(Auto_timer.Get() > 1.5){
+      _channel.setState(Channel::State::Shooting);
+      }
+  }
+
+  //intake Button X
+  else if(xbox.GetRawButton(3)){
+    _intake.setState(Intake::State::Deploy);
+    _channel.setState(Channel::State::Intake);
+    _shooter.setState(Shoot::State::Idle);
+  } 
+
+  //Manual Zero
+  else if(xbox.GetBackButton()){
+    _shooter.Manual_Zero();
+  }
+
+  //Climb
+  else if(l_joy.GetTrigger() && r_joy.GetTrigger()){
+    
+  }
+
+  else {
+    _intake.setState(Intake::State::Idle);
+    _shooter.setState(Shoot::State::Idle);
+    _channel.setState(Channel::State::Idle);
+  }  
+  if (!xbox.GetRawButton(2)) {
+    Auto_timer.Reset();
+    Auto_timer.Stop();
+  }
+
+  _shooter.Periodic();
+  _channel.Periodic();
+  _intake.Periodic();
+}
+
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+void Robot::TestInit() {}
+
+
+//Shooter Calibration
+void Robot::TestPeriodic() {
+  _shooter.Manual_Turret(xbox.GetX(frc::GenericHID::kRightHand));
+  _drivetrain.Periodic(l_joy.GetRawAxis(1), -1*r_joy.GetRawAxis(0));
+  
+  //Shoot Button B
+  if(xbox.GetRawButton(2)) {
+    _shooter.setState(Shoot::State::Calibrate);
+    _channel.setState(Channel::State::Idle);
+    Auto_timer.Start();
+      if(Auto_timer.Get() > 1.5){
+      _channel.setState(Channel::State::Shooting);
+      }
+  }
+
+  else if(xbox.GetRawButton(1)){
+    _shooter.Turret_Calibrate();
+  }
+
+  else {
+    _intake.setState(Intake::State::Idle);
+    _shooter.setState(Shoot::State::Idle);
+    _channel.setState(Channel::State::Idle);
+  }  
+  if (!xbox.GetRawButton(2)) {
+    Auto_timer.Reset();
+    Auto_timer.Stop();
+  }
+
+  _shooter.Periodic();
+  _channel.Periodic();
+  _intake.Periodic();
+}
+
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ 2021_v2/2021/src/main/cpp/Shoot.cpp
@@ -0,0 +1,238 @@
+#include "Shoot.h"
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/Timer.h>
+#include <string>
+#include <map>
+
+
+//Shooter constructor
+Shoot::Shoot(){
+    //Configure motors
+    shoot_slave->Follow(*shoot_master);
+    turret->ConfigMotionCruiseVelocity(500);
+    turret->ConfigMotionAcceleration(500);
+    turret->SetExpiration(30);
+    shoot_slave->SetExpiration(30);
+    shoot_master->SetExpiration(30);
+
+    //Hash map for hood and flywheel values
+    dataMap[-3.8] = {0.47, 0.95};
+	dataMap[-1.2] = {0.44, 0.95};
+	dataMap[1.0] = {0.45, 0.92};
+	dataMap[3.0] = {0.44, 0.92};
+	dataMap[4.57] = {0.43, 0.92};
+	dataMap[5.47] = {0.42, 0.92};
+	dataMap[9.5] = {0.41, 0.92};
+    dataMap[14.06] = {0.40, 0.90};
+    dataMap[18.2] = {0.20, 0.90};
+    dataMap[20.1] = {0.12, 0.90};
+    dataMap[22.4] = {0.0, 0.90};
+}
+
+
+void Shoot::Periodic(){
+    turret->SetNeutralMode(NeutralMode::Brake);
+    switch(state){
+        case State::Idle:
+            limelight->setLEDMode("ON");
+            shoot_master->Set(ControlMode::PercentOutput, 0.0);
+            shoot_slave->Set(ControlMode::PercentOutput, 0.0);
+            break;
+        case State::Aiming:
+            limelight->setLEDMode("ON");
+            Aim();
+            break;
+        case State::Shooting:
+            limelight->setLEDMode("ON");
+            Its_gonna_shoot();
+        case State::Calibrate:
+            Shooter_Calibrate();
+        default:
+            break;
+    }    
+}
+
+
+//Set shooter states
+void Shoot::setState(Shoot::State newState) {
+    state = newState;
+}
+
+
+//Shoot - Find values from the limelight and set shooter values
+bool Shoot::Its_gonna_shoot(){
+    double point = limelight->getYOff();
+	double angle, speed;
+
+	auto data = dataMap.find(point);
+	if (data != dataMap.end()) { 
+		// point is in dataMap. get exact angle and speed from hash map
+		angle = data->second.first;
+		speed = data->second.second;
+	} else { 
+	  // point is not in dataMap. Find 2 closest points for interploation
+	  double point1, point2;
+		if (interpolate(dataPoints, point, point1, point2)) {
+			// Found 2 points for interpolation
+			auto data1 = dataMap[point1];
+			double angle1, speed1;
+			angle1 = data1.first;
+			speed1 = data1.second;
+			
+			auto data2 = dataMap[point2];
+			double angle2, speed2;
+			angle2 = data2.first;
+			speed2 = data2.second;
+
+			// interpolate
+			angle = (angle1 + angle2) / 2;
+			speed = (speed1 + speed2) / 2;
+
+		} else {
+			return false;
+		}
+	}
+    //std::cout << "flywheel output: " << speed << std::endl;
+    shoot_master->Set(ControlMode::PercentOutput, speed);
+    shoot_slave->Set(ControlMode::PercentOutput, -speed);
+    servo_left.Set(angle);
+    servo_right.Set(angle);
+    return true;
+}
+
+
+//Aim the turret to the goal
+int prev_xoff = 0;
+void Shoot::Aim() {
+    //READING_SDB_NUMERIC(double, Turret_P) TKp;
+    //READING_SDB_NUMERIC(double, Turret_I) TKi;
+    //READING_SDB_NUMERIC(double, Turret_D) TKd;
+    TKp = 0.015;
+    TKi = -0.013;
+    TKd = 0.009;
+	x_off = limelight->getXOff();
+    double power;
+    // if(x_off > 500){
+    //     if (turret->GetSelectedSensorPosition() < -19870.0/2.0) power = 0.25;
+    //     if (turret->GetSelectedSensorPosition() > -19870.0/2.0) power = -0.25;
+    // }
+    if (x_off > 500) {
+        power = 0;
+    }
+    double delta_xoff = (x_off - prev_xoff);
+	power = TKp*x_off + TKi + TKd*delta_xoff;
+    prev_xoff = x_off;
+    
+    if (power < -0.5) power = -0.5;
+    if (power > 0.5) power = 0.5;
+    if((turret->GetSelectedSensorPosition() > 0 ) || (turret->GetSelectedSensorPosition() < -19870)){
+        power = 0;
+    }
+    turret->Set(ControlMode::PercentOutput, power);
+}
+
+
+double Shoot::GetLimelightY(){
+    return limelight->getYOff();
+}
+
+double Shoot::GetLimelightX(){
+    return limelight->getXOff();
+}
+
+void Shoot::Auto(){
+    
+}
+
+
+void Shoot::Zero(){
+    turret->SetSelectedSensorPosition(0);
+    turret->Set(ControlMode::PercentOutput, 0);
+    //Please work
+    while(turret->GetSelectedSensorPosition() > -9500){
+        turret->Set(ControlMode::PercentOutput, -0.1);
+    }
+    turret->Set(ControlMode::PercentOutput, 0);
+}
+
+
+void Shoot::Manual_Zero(){
+    while(turret_limit_switch->Get()){
+        turret->Set(ControlMode::PercentOutput, 0.12);
+    }
+    turret->Set(ControlMode::PercentOutput, 0);
+    turret->SetSelectedSensorPosition(0);
+}
+
+
+//Operator movement
+void Shoot::Manual_Turret(double turret_rot){
+    if(abs(turret_rot) <= 0.05){
+        turret_rot = 0;
+    }
+    if(turret_rot > 0 && turret->GetSelectedSensorPosition() > 0){
+        turret_rot = 0;
+    }
+    if(turret_rot < 0 && turret->GetSelectedSensorPosition() < -19870){
+        turret_rot = 0;
+    }
+    turret->Set(ControlMode::PercentOutput, turret_rot*0.12);
+}
+
+
+bool Shoot::interpolate(std::vector<double>& array, double p, double& p1, double& p2){
+    int left = 0, right = array.size()-1;
+
+	if (p < array[left] || p > array[right]) {
+		return false;
+	}
+
+	int mid = (left+right)/2;
+
+	while (left < right) {
+		if (array[mid] <= p && p <= array[mid+1]) {
+			p1 = array[mid];
+			p2 = array[mid+1];
+			return true;
+		} else if (array[mid] <= p) {
+			left = mid;
+		} else {
+			right = mid;
+		}
+		mid = (left+right)/2;
+	}
+	return false;
+}
+
+
+//Testing function for calibrating shooter values
+void Shoot::Shooter_Calibrate(){
+    READING_SDB_NUMERIC(double, Hood_angle_out) hood_out;
+    READING_SDB_NUMERIC(double, Flywheel_percent_out) flywheel_out;
+
+    shoot_master->Set(ControlMode::PercentOutput, flywheel_out);
+    shoot_slave->Set(ControlMode::PercentOutput, -flywheel_out);
+    servo_left.Set(hood_out);
+    servo_right.Set(hood_out);
+}
+
+void Shoot::Turret_Calibrate(){
+    READING_SDB_NUMERIC(double, Turret_P) TKp;
+    READING_SDB_NUMERIC(double, Turret_I) TKi;
+    READING_SDB_NUMERIC(double, Turret_D) TKd;
+    x_off = limelight->getXOff();
+    double power;
+    if (x_off > 500) {
+        power = 0;
+    }
+    double delta_xoff = (x_off - prev_xoff);
+	power = TKp*x_off + TKi + TKd*delta_xoff;
+    prev_xoff = x_off;
+    
+    if (power < -0.5) power = -0.5;
+    if (power > 0.5) power = 0.5;
+    if((turret->GetSelectedSensorPosition() > 0 ) || (turret->GetSelectedSensorPosition() < -19870)){
+        power = 0;
+    }
+    turret->Set(ControlMode::PercentOutput, power);
+}
