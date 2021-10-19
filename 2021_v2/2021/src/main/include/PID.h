932c0d5de359b22233e663cba396062d1ddc134a
AndrewKim1S <54419795+AndrewKim1S@users.noreply.github.com>
2021-10-19 15:42:25 -0700

    Delete 2021_v2/2021 directory

--- 2021_v2/2021/src/main/include/PID.h
+++
@@ -1,8 +0,0 @@
-
-class PID {
-    public:
-        PID(double Kp, double Ki, double Kd, double min_out, double max_out);
-        double getPower(double error, double sp);
-    private:
-        double Kp, Ki, Kd, min, max;
-};
