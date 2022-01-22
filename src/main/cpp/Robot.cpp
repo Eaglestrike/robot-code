b3b64c46c065675efce42ef30efa7117c1eb4cc3
Susan Clay <47490118+SusanC3@users.noreply.github.com>
2022-01-21 18:16:51 -0800

    Add files via upload

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,54 +1,45 @@
+
+//https://docs.photonvision.org/en/latest/docs/programming/photonlib
+//needs photonlib and ctre libraries
+
+
 #include "Robot.h"
+#include <fmt/core.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <iostream>
 
-void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-}
 
-void Robot::RobotPeriodic() {
-  compressor.Start();
-}
 
-void Robot::AutonomousInit() {}
-void Robot::AutonomousPeriodic() {
-  _drivetrain.Auto();
-}
 void Robot::TeleopInit() {
-  _shooter.Zero();
+  camera.SetDriverMode(false); // we want target tracking
+  camera.SetLEDMode(photonlib::LEDMode::kOn);
+  camera.SetPipelineIndex(1); //test is, as far as i know, better than Reflective-Test
 }
+
 
 void Robot::TeleopPeriodic() {
-  _drivetrain.Periodic(l_joy.GetRawAxis(1), -1*r_joy.GetRawAxis(0));
-  _shooter.Manual_Turret(xbox.GetX(frc::GenericHID::kRightHand));
+  photonlib::PhotonPipelineResult result = camera.GetLatestResult();
+  
+  if (result.HasTargets()) {
+    photonlib::PhotonTrackedTarget target = result.GetBestTarget();
+    double yaw = target.GetYaw(); //horizontal offset (in degrees, neg is left)
+    double pitch = target.GetPitch(); //vertical offset (in degrees, neg is bottom)
+    double area = target.GetArea(); //0 to 100 percent of screen
+    double skew = target.GetSkew(); //rotation? counter clockwise positive, from horizontal
 
-   _drivetrain.SetWantCheesyDrive(-l_joy.GetRawAxis(1), r_joy.GetRawAxis(0), r_joy.GetRawButton(1));
+    //distance to goal (these heights and angle are for 2022 comp)
+    units::meter_t range = photonlib::PhotonUtils::CalculateDistanceToTarget(
+      units::length::meter_t(18_in), units::length::meter_t(104_in), units::angle::radian_t(0.872665_rad), units::degree_t{pitch}); 
 
-  //Button 1 is A
-  if(xbox.GetRawButton(1)){
-      _shooter.setState(Shoot::State::Aiming);
-      _shooter.Periodic(); //shouldn't this be outside an if statement??
-  } else {
-      _shooter.setState(Shoot::State::Idle);
-  }
+    //there's also a function that spits out a pose2d? that is like where you are on the field. might investiage that
 
-  //Button 2 is B?
-  if(xbox.GetRawButton(2)){
-      _channel.Run();
-      _shooter.Shooter_Calibrate();
-  } else {
-      _channel.Stop();
-      _shooter.setState(Shoot::State::Idle);
-      _shooter.Periodic();
-  }
+    std::cout << "yaw: " << yaw << ", pitch: " << pitch << ", area: " << area << ", skew: " << skew << "\n";
+   // std::cout << "distance to goal: "<< range.value() << "\n";
+    std::cout << "\n";
+
+  } else std::cout << "No targets\n";
+
 }
-
-void Robot::DisabledInit() {}
-void Robot::DisabledPeriodic() {}
-void Robot::TestInit() {}
-void Robot::TestPeriodic() {
-}
-
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,46 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 #pragma once
 
 #include <string>
+
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
-#include <frc/smartdashboard/SmartDashboard.h>
-#include <frc/Joystick.h>
-#include <frc/XboxController.h>
-#include <frc/Timer.h>
-#include <frc/Compressor.h>
-#include "Drive.h"
-#include "Shoot.h"
-#include "Intake.h"
-#include "Channel.h"
-#include "Climb.h"
+#include <photonlib/PhotonCamera.h>
+#include <photonlib/PhotonUtils.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
 
-frc::Joystick l_joy{0};
-frc::Joystick r_joy{1};
-frc::XboxController xbox{2};
-frc::Compressor compressor{0};
 
 class Robot : public frc::TimedRobot {
-  public:
-    void RobotInit() override;
-    void RobotPeriodic() override;
-    void AutonomousInit() override;
-    void AutonomousPeriodic() override;
-    void TeleopInit() override;
-    void TeleopPeriodic() override;
-    void DisabledInit() override;
-    void DisabledPeriodic() override;
-    void TestInit() override;
-    void TestPeriodic() override;
+ public:
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  
 
-  private:
-    frc::SendableChooser<std::string> m_chooser;
-    const std::string kAutoNameDefault = "Default";
-    const std::string kAutoNameCustom = "My Auto";
-    std::string m_autoSelected;
-
-    Drive _drivetrain;
-    Shoot _shooter;
-    Intake _intake;
-    Channel _channel;
-    Climb _climb;
+ private:
+  photonlib::PhotonCamera camera{"gloworm"};
 };
